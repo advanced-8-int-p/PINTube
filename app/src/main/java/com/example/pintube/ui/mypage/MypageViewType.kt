f7f9779b340485bdf83d9c1c76a45b0e43ce67b0
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-02-11 05:32:27 +0900

    Feat : mypage recyclerview

--- app/src/main/java/com/example/pintube/ui/mypage/MypageViewType.kt
+++ app/src/main/java/com/example/pintube/ui/mypage/MypageViewType.kt
@@ -1,5 +1,15 @@
 package com.example.pintube.ui.mypage
 
+import com.example.pintube.domain.entitiy.VideoEntity
+import com.example.pintube.ui.home.VideoItemData
+
 sealed class MypageViewType {
 
+    data class Profile(val userId: String): MypageViewType()
+
+    data class Header(val title: String): MypageViewType()
+
+    data class RecentItems(val data: VideoItemData): MypageViewType()
+
+    data class PinItems(val data: VideoItemData): MypageViewType()
 }
