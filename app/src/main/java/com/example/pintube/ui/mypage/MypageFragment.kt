7cde6c71d99b62ffad9bdf3a7054a0d9a6d53543
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-02-14 21:09:31 +0900

    Feat : mypage 리사이클러뷰 데이터 받기 하는중

--- app/src/main/java/com/example/pintube/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/pintube/ui/mypage/MypageFragment.kt
@@ -9,21 +9,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation.findNavController
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import com.example.pintube.R
+import com.example.pintube.data.local.entity.LocalVideoEntity
 import com.example.pintube.databinding.FragmentMypageBinding
+import com.example.pintube.domain.entitiy.VideoEntity
+import com.example.pintube.domain.repository.LocalFavoriteRepository
+import com.example.pintube.domain.repository.LocalVideoRepository
 import com.example.pintube.ui.Search.SearchActivity
-import com.example.pintube.ui.main.MainActivity
-import com.example.pintube.utill.convertViewCount
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -31,9 +30,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.handleCoroutineException
+import kotlinx.coroutines.launch
 
-class MypageFragment : Fragment() {
+class MypageFragment(
+    private val localFavoriteRepository: LocalFavoriteRepository,
+    private val localVideoRepository: LocalVideoRepository
+) : Fragment() {
 
     private lateinit var mContext: Context
 
@@ -59,15 +61,22 @@ class MypageFragment : Fragment() {
 
     private var isLoggedIn: Boolean = false
 
-    private val mItems: MutableList<MypageViewType>
-        get() {
-            return mutableListOf<MypageViewType>(
-                MypageViewType.Header("최근 시청 영상", true),
-                MypageViewType.RecentItems(RecyclerviewRecentVideoAdapter()),
-                MypageViewType.Header("저장한 동영상", false),
-                MypageViewType.PinItems(RecyclerviewPinnedGroupAdapter())
-            )
-        }
+//    private val localFavoriteRepository: LocalFavoriteRepository
+
+    private var favIdList: List<String> = listOf()
+
+    private var favVideoList: List<LocalVideoEntity> = listOf()
+
+
+    private lateinit var mItems: MutableList<MypageViewType>
+//        get() {
+//            return mutableListOf<MypageViewType>(
+//                MypageViewType.Header("최근 시청 영상", true),
+//                MypageViewType.RecentItems(RecyclerviewRecentVideoAdapter()),
+//                MypageViewType.Header("저장한 동영상", false),
+//                MypageViewType.PinItems(RecyclerviewPinnedGroupAdapter(favIdList))
+//            )
+//        }
 
     private val adapter by lazy { MypageAdapter(mContext, mItems) }
 
@@ -85,7 +94,7 @@ class MypageFragment : Fragment() {
         val account = GoogleSignIn.getLastSignedInAccount(requireContext())
         account?.let {
             isLoggedIn = true
-            Snackbar.make(binding.mypageFragment, "logged in", Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(binding.mypageFragment, "로그인 상태입니다", Snackbar.LENGTH_SHORT).show()
         }
 
     }
@@ -106,8 +115,17 @@ class MypageFragment : Fragment() {
 //        mypageViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
-        initView()
 
+        getFavoriteVideos()
+
+        mItems = mutableListOf<MypageViewType>(
+            MypageViewType.Header("최근 시청 영상", true),
+            MypageViewType.RecentItems(RecyclerviewRecentVideoAdapter()),
+            MypageViewType.Header("저장한 동영상", false),
+            MypageViewType.PinItems(RecyclerviewPinnedGroupAdapter(favVideoList))
+        )
+
+        initView()
 
         return root
     }
@@ -157,6 +175,7 @@ class MypageFragment : Fragment() {
             //배경..... 유튜브 채널 헤더를 어디서 가져오지 아니 근데 구글 계정마다 다 채널 있지는 않지 않나 음...
             b.mypageFragment.setBackgroundResource(R.drawable.hamster)
             b.tvMypageChannelName.text = myProfileData.myAccountName
+            //id가 id가 아니라 그 숫자고유값?그런거같은
             b.tvMypageChannelId.text = myProfileData.myAccountId.toString()
 
 //            isLoggedIn = (myProfileData.myAccountId != null)
@@ -186,6 +205,8 @@ class MypageFragment : Fragment() {
                 signIn()
                 getCurrentUserProfile()
 
+                onResume()
+
             }
 
             b.tvMypageChannelLogout.setOnClickListener {
@@ -195,6 +216,8 @@ class MypageFragment : Fragment() {
                 myProfileData.myAccountName = null
                 signOut()
                 revokeAccess()
+
+                onResume()
             }
 
             getCurrentUserProfile()
@@ -211,11 +234,16 @@ class MypageFragment : Fragment() {
                 val account = task.getResult(ApiException::class.java)
                 myProfileData.myAccountProfileUri = account.photoUrl.toString()
                 myProfileData.myAccountName = account.displayName
-                myProfileData.myAccountId = account.id
+                myProfileData.myAccountId = account.email
             } catch (e: ApiException) {
                 Log.d("googleLogin", e.toString())
             }
         }
+    }
+
+    private fun getFavoriteVideos() = lifecycleScope.launch {
+        favIdList = localFavoriteRepository.findCategoryVideos("기본")
+//        favVideoList = favIdList.let { localVideoRepository.findVideoDetail(it) }
     }
 
 
@@ -292,11 +320,6 @@ class MypageFragment : Fragment() {
 
             Log.d("login", id)
             Log.d("login", myProfileData.myAccountId.toString())
-            Log.d("login", displayName)
-            Log.d("login", myProfileData.myAccountName.toString())
-            Log.d("login", photoUrl)
-            Log.d("login", myProfileData.myAccountProfileUri.toString())
-            //whyyyyyyyyyyyy.......
         }
     }
 
--- app/src/main/java/com/example/pintube/ui/mypage/MypageViewType.kt
+++ app/src/main/java/com/example/pintube/ui/mypage/MypageViewType.kt
@@ -1,9 +1,5 @@
 package com.example.pintube.ui.mypage
 
-import com.example.pintube.data.local.dao.ChannelThumbnail
-import com.example.pintube.domain.entitiy.VideoEntity
-import com.example.pintube.ui.home.VideoItemData
-
 sealed class MypageViewType {
 
 //    data class Profile(val myProfile: MypageProfileData) : MypageViewType()
@@ -11,7 +7,6 @@ sealed class MypageViewType {
     data class Header(val title: String, val isRecent: Boolean) : MypageViewType()
 
     data class RecentItems(
-//        val items: MutableList<RecentItem>,
         val recentAdapter: RecyclerviewRecentVideoAdapter
     ) : MypageViewType()
 
