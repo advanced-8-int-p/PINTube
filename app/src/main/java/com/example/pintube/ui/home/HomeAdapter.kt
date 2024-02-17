9ccc973781353f5d98ffdc046aef2cb0059a7059
jericho31 <a@a.a>
2024-02-18 01:53:34 +0900

    refactor: SealedMulti -> 홈어댑터 파일로 위치 통합

--- app/src/main/java/com/example/pintube/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/pintube/ui/home/HomeAdapter.kt
@@ -15,6 +15,55 @@ import com.example.pintube.databinding.ItemHeaderBinding
 import com.example.pintube.databinding.ItemLoadingProgressBinding
 import com.example.pintube.databinding.VideoItemBinding
 
+sealed interface SealedMulti {
+
+//    enum class Type {
+//        MULTI_POPULAR,
+//        MULTI_CATEGORY,
+//        MULTI_VIDEO,
+//        MULTI_HEADER,
+//        MULTI_LOADING,
+//    }
+
+    data object Type {
+        const val popular = 1
+        const val category = 2
+        const val video = 3
+        const val header = 4
+        const val loading = 5
+    }
+
+    val viewType: Int
+
+    data object Header : SealedMulti {
+        override val viewType: Int = Type.header
+    }
+
+    data class Popular(
+        val videoAdapter: PopularVideoAdapter
+    ) : SealedMulti {
+        override val viewType: Int = Type.popular
+    }
+
+    data class Category(
+        val categoryAdapter: CategoryAdapter,
+    ) : SealedMulti {
+        override val viewType: Int = Type.category
+    }
+
+    data class Video(
+        val videoItemData: VideoItemData,
+    ) : SealedMulti {
+        override val viewType: Int = Type.video
+    }
+
+    data object Loading : SealedMulti {
+        override val viewType: Int = Type.loading
+    }
+
+}
+
+
 class HomeAdapter(
     private val onCategorySettingClick: () -> Unit,
     private val onVideoClick: (item: VideoItemData) -> Int?
